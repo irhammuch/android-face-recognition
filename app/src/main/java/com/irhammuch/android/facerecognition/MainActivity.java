15e4f043e5220036cdd2947d436192a088d2aa22
irhammuch <muchamad.irham@gmail.com>
2022-02-26 15:06:11 +0700

    Setup Camera & MLKit Face Detection

--- app/src/main/java/com/irhammuch/android/facerecognition/MainActivity.java
+++ app/src/main/java/com/irhammuch/android/facerecognition/MainActivity.java
@@ -1,14 +1,186 @@
 package com.irhammuch.android.facerecognition;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "facerecognition";
+    private static final int PERMISSION_CODE = 1001;
+    private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
+    private PreviewView previewView;
+    private CameraSelector cameraSelector;
+    private ProcessCameraProvider cameraProvider;
+    private Preview previewUseCase;
+    private ImageAnalysis analysisUseCase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        previewView = findViewById(R.id.previewView);
+        previewView.setScaleType(PreviewView.ScaleType.FILL_CENTER);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startCamera();
+    }
+
+    public void startCamera() {
+        if(ContextCompat.checkSelfPermission(this, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+            setupCamera();
+        } else {
+            getPermissions();
+        }
+    }
+
+    private void getPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA_PERMISSION}, PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int[] grantResults) {
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        if (requestCode == PERMISSION_CODE) {
+            setupCamera();
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void setupCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                ProcessCameraProvider.getInstance(this);
+
+        int lensFacing = CameraSelector.LENS_FACING_BACK;
+        cameraSelector = new CameraSelector.Builder().requireLensFacing(lensFacing).build();
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                cameraProvider = cameraProviderFuture.get();
+                bindAllCameraUseCases();
+            } catch (ExecutionException | InterruptedException e) {
+                Log.e(TAG, "cameraProviderFuture.addListener Error", e);
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindAllCameraUseCases() {
+        if (cameraProvider != null) {
+            cameraProvider.unbindAll();
+            bindPreviewUseCase();
+            bindAnalysisUseCase();
+        }
+    }
+
+    private void bindPreviewUseCase() {
+        if (cameraProvider == null) {
+            return;
+        }
+
+        if (previewUseCase != null) {
+            cameraProvider.unbind(previewUseCase);
+        }
+
+        Preview.Builder builder = new Preview.Builder();
+        builder.setTargetAspectRatio(AspectRatio.RATIO_4_3);
+        builder.setTargetRotation(getRotation());
+
+        previewUseCase = builder.build();
+        previewUseCase.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        try {
+            cameraProvider
+                    .bindToLifecycle(this, cameraSelector, previewUseCase);
+        } catch (Exception e) {
+            Log.e(TAG, "Error when bind preview", e);
+        }
+    }
+
+    private void bindAnalysisUseCase() {
+        if (cameraProvider == null) {
+            return;
+        }
+
+        if (analysisUseCase != null) {
+            cameraProvider.unbind(analysisUseCase);
+        }
+
+        Executor cameraExecutor = Executors.newSingleThreadExecutor();
+
+        ImageAnalysis.Builder builder = new ImageAnalysis.Builder();
+        builder.setTargetAspectRatio(AspectRatio.RATIO_4_3);
+        builder.setTargetRotation(getRotation());
+
+        analysisUseCase = builder.build();
+        analysisUseCase.setAnalyzer(cameraExecutor, this::analyze);
+
+        try {
+            cameraProvider
+                    .bindToLifecycle(this, cameraSelector, analysisUseCase);
+        } catch (Exception e) {
+            Log.e(TAG, "Error when bind analysis", e);
+        }
+    }
+
+    protected int getRotation() throws NullPointerException {
+        return previewView.getDisplay().getRotation();
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private void analyze(@NonNull ImageProxy image) {
+        if (image.getImage() == null) return;
+
+        InputImage inputImage = InputImage.fromMediaImage(
+                image.getImage(),
+                image.getImageInfo().getRotationDegrees()
+        );
+
+        FaceDetector faceDetector = FaceDetection.getClient();
+
+        faceDetector.process(inputImage)
+                .addOnSuccessListener(faces -> onSuccessListener(faces, inputImage, image.getImageInfo().getRotationDegrees()))
+                .addOnFailureListener(e -> Log.e(TAG, "Barcode process failure", e))
+                .addOnCompleteListener(task -> image.close());
+    }
+
+    private void onSuccessListener(List<Face> faces, InputImage image, int rotation) {
+        if(faces.size() > 0) {
+            Toast.makeText(this, "Face detected", Toast.LENGTH_SHORT).show();
+        }
     }
 }
